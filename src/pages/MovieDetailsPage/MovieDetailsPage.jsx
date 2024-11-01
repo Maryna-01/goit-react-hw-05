df4c694f24cce5187cc3206650c6dcc58063d8d0
unknown <liashchenkomarina@gmal.com>
2024-11-02 00:23:03 +0100

    task

---
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef, useState } from "react";
+import { useParams, Outlet, Link, useLocation } from "react-router-dom";
+import { fetchMovieDetails } from "../../api";
+
+const MovieDetailsPage = () => {
+    const { movieId } = useParams();
+    const [movie, setMovie] = useState(null);
+    const location = useLocation();
+    const prevLocationRef = useRef(location.state?.from);
+
+    useEffect(() => {
+        fetchMovieDetails(movieId).then(setMovie);
+    }, [movieId]);
+
+    if (!movie) return <div>Loading...</div>;
+
+    return (
+        <div>
+            <Link to={prevLocationRef.current || "/"}>Go Back</Link>
+            <h1>{movie.title}</h1>
+            <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={movie.title}
+            />
+            <p>{movie.overview}</p>
+            <ul>
+                <li>
+                    <Link to="cast">Cast</Link>
+                </li>
+                <li>
+                    <Link to="reviews">Reviews</Link>
+                </li>
+            </ul>
+            <Outlet />
+        </div>
+    );
+};
+
+export default MovieDetailsPage;
+
+
---
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { searchMovies } from "../api";
+import MovieList from "../components/MovieList";
+import { useSearchParams } from "react-router-dom";
+
+const MoviesPage = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [query, setQuery] = useState(searchParams.get("query") || "");
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            if (query) {
+                const results = await searchMovies(query);
+                setMovies(results);
+            }
+        };
+        fetchMovies();
+    }, [query]);
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const newQuery = e.target.search.value;
+        setQuery(newQuery);
+        setSearchParams({ query: newQuery });
+    };
+
+    return (
+        <div>
+            <h1>Search Movies</h1>
+            <form onSubmit={handleSearch}>
+                <input
+                    type="text"
+                    name="search"
+                    placeholder="Search for a movie"
+                    defaultValue={query}
+                />
+                <button type="submit">Search</button>
+            </form>
+            {query && <MovieList movies={movies} />}
+        </div>
+    );
+};
+
+export default MoviesPage;
